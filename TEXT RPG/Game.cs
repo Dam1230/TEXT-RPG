6c67647b59449e54a5d24e2a6d11c8de9f090d12
Dam1230 <dkssud4111@gmail.com>
2025-04-08 14:13:30 +0900

    초기 커밋.

---
+++ TEXT RPG/Game.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEXT_RPG
+{
+    public static class Game
+    {
+        private static Dictionary<string, BaseScene> sceneDic;
+        private static BaseScene curScene;
+
+        private static bool gameOver;
+
+        public static void Run()
+        {
+            Start();
+            while (gameOver == false)
+            {
+                Console.Clear();
+                curScene.Render();
+                curScene.Input();
+                curScene.Update();
+                curScene.Result();
+            }
+            End();
+        }
+
+        //게임의 시작 작업 진행
+        private static void Start()
+        {
+            sceneDic = new Dictionary<string, BaseScene>();
+            sceneDic.Add("Title", new TitleScene());
+            gameOver = false;
+
+            curScene = sceneDic["Title"];
+        }
+
+        //게임의 마무리 작업 진행
+        public static void End()
+        {
+            sceneDic.Clear();
+            curScene = null;
+            gameOver = true;
+        }
+    }
+}
---
+++ TEXT RPG/TitleScene.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEXT_RPG
+{
+    public class TitleScene : BaseScene
+    {
+        //⌝⌜⌟⌞◜◝◝◜◞◟‾_–‾—=–−-
+        public override void Render()
+        {
+            Console.WriteLine("--------------------");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("      TEXT RPG      ");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("--------------------");
+        }
+        public override void Input()
+        {
+            Console.ReadKey();
+        }
+        public override void Update()
+        {
+
+        }
+        public override void Result()
+        {
+
+        }
+    }
+
+}
+
